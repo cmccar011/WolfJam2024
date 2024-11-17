a553b693290eb8c947013cbbf58da7a9e0dcc167
GamingGabriel <gabrieljrosss@gmail.com>
2024-11-16 21:46:15 -0500

    Death Update
    Things Die

--- Assets/scripts/knifeScript.cs
+++ Assets/scripts/knifeScript.cs
@@ -38,4 +38,12 @@ public class knifeScript : MonoBehaviour
     {
         
     }
+
+    private void OnCollisionEnter2D(Collision2D other)
+        {
+            if (other.gameObject.CompareTag("Enemy"))
+            {
+                Destroy(gameObject);
+            }
+        }
 }
